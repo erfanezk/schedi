6ac847f2e95a81705c1482b74e390b9fa04678bc
erfan <erfanrunc@gmail.com>
2025-02-06 11:46:06 +0330

    enhancement: store onceTime, interval and cron tasks

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,30 +1,101 @@
 import Dexie from 'dexie';
-import { ICronTask, ITask, ITaskDatabase } from '@/interfaces';
+import { ICronTask, IIntervalTask, IOnceTimeTask, ITaskDatabase } from '@/interfaces';
+import { CommonUtils } from '@/utils';
 
 const DB_VERSION = 1;
 
 class TaskDatabase extends Dexie implements ITaskDatabase {
-  cronTasks!: Dexie.Table<ICronTask, number>;
-  periodicTasks!: Dexie.Table<ITask, number>;
-  intervalTasks!: Dexie.Table<ITask, number>;
+  cronTasks!: Dexie.Table<ICronTask, string>;
+  onceTimeTasks!: Dexie.Table<IOnceTimeTask, string>;
+  intervalTasks!: Dexie.Table<IIntervalTask, string>;
 
   constructor() {
     super('TaskSchedulerDB');
     this.version(DB_VERSION).stores({
-      cronTasks: '++id, name, schedule, createdAt, status',
-      periodicTasks: '++id, name, schedule, createdAt, status',
-      intervalTasks: '++id, name, schedule, createdAt, status',
+      cronTasks: 'id, name, createdAt, enabled, dateChanged, schedule, lastRunAt, totalRunCount',
+      onceTimeTasks: 'id, name, createdAt, enabled, dateChanged, runAt',
+      intervalTasks:
+        'id, name, createdAt, enabled, dateChanged, interval, lastRunAt, totalRunCount',
     });
 
     this.cronTasks = this.table('cronTasks');
-    this.periodicTasks = this.table('periodicTasks');
+    this.onceTimeTasks = this.table('onceTimeTasks');
     this.intervalTasks = this.table('intervalTasks');
   }
 
-  addCronTask(task: Omit<ICronTask, 'id'>): Promise<ICronTask> {
-    return new Promise((resolve) => {
-      resolve({ id: '1', ...task });
+  async addCronTask(task: Omit<ICronTask, 'id'>): Promise<ICronTask> {
+    const id = await this.cronTasks.add({
+      ...task,
+      createdAt: new Date().getTime(),
+      id: CommonUtils.generateUniqueId(),
     });
+    return { id, ...task };
+  }
+
+  async addIntervalTask(task: Omit<IIntervalTask, 'id'>): Promise<IIntervalTask> {
+    const id = await this.intervalTasks.add({
+      ...task,
+      createdAt: new Date().getTime(),
+      id: CommonUtils.generateUniqueId(),
+    });
+    return { id, ...task };
+  }
+
+  async addOnceTimeTask(task: Omit<IOnceTimeTask, 'id'>): Promise<IOnceTimeTask> {
+    const id = await this.onceTimeTasks.add({
+      ...task,
+      createdAt: new Date().getTime(),
+      id: CommonUtils.generateUniqueId(),
+    });
+    return { id, ...task };
+  }
+
+  async getAllCronTasks(): Promise<ICronTask[]> {
+    return this.cronTasks.toArray();
+  }
+
+  async getAllIntervalTasks(): Promise<IIntervalTask[]> {
+    return this.intervalTasks.toArray();
+  }
+
+  async getAllOnceTimeTasks(): Promise<IOnceTimeTask[]> {
+    return this.onceTimeTasks.toArray();
+  }
+
+  async getCronTaskById(id: string): Promise<ICronTask | undefined> {
+    return this.cronTasks.get(id);
+  }
+
+  async getIntervalTaskById(id: string): Promise<IIntervalTask | undefined> {
+    return this.intervalTasks.get(id);
+  }
+
+  async getOnceTimeTaskById(id: string): Promise<IOnceTimeTask | undefined> {
+    return this.onceTimeTasks.get(id);
+  }
+
+  async updateCronTask(id: string, updates: Partial<ICronTask>): Promise<void> {
+    await this.cronTasks.update(id, updates);
+  }
+
+  async updateIntervalTask(id: string, updates: Partial<IIntervalTask>): Promise<void> {
+    await this.intervalTasks.update(id, updates);
+  }
+
+  async updateOnceTimeTask(id: string, updates: Partial<IOnceTimeTask>): Promise<void> {
+    await this.onceTimeTasks.update(id, updates);
+  }
+
+  async deleteCronTask(id: string): Promise<void> {
+    await this.cronTasks.delete(id);
+  }
+
+  async deleteIntervalTask(id: string): Promise<void> {
+    await this.intervalTasks.delete(id);
+  }
+
+  async deleteOnceTimeTask(id: string): Promise<void> {
+    await this.onceTimeTasks.delete(id);
   }
 }
 
--- src/interfaces/tasks.ts
+++ src/interfaces/tasks.ts
@@ -6,24 +6,60 @@ enum TaskType {
 
 interface ITask {
   id: string;
+  name: string;
+  createdAt: number;
+  enabled: boolean;
+  dateChanged: number;
 }
 
-interface ICronTask extends ITask {}
+interface ICronTask extends ITask {
+  schedule: string;
+  lastRunAt: number;
+  totalRunCount: number;
+}
+
+interface IOnceTimeTask extends ITask {
+  runAt: number;
+}
+
+interface IIntervalTask extends ITask {
+  interval: number;
+  lastRunAt: number;
+  totalRunCount: number;
+}
 
 interface ITaskDatabase {
   addCronTask(task: Omit<ICronTask, 'id'>): Promise<ICronTask>;
 
-  addPeriodicTask(task: Omit<ITask, 'id'>): Promise<ITask>;
+  addOnceTimeTask(task: Omit<IOnceTimeTask, 'id'>): Promise<IOnceTimeTask>;
 
-  addIntervalTask(task: Omit<ITask, 'id'>): Promise<ITask>;
+  addIntervalTask(task: Omit<IIntervalTask, 'id'>): Promise<IIntervalTask>;
 
-  getAllCronTasks(): Promise<ITask[]>;
+  getAllCronTasks(): Promise<ICronTask[]>;
 
-  getAllPeriodicTasks(): Promise<ITask[]>;
+  getAllOnceTimeTasks(): Promise<IOnceTimeTask[]>;
 
-  getAllIntervalTasks(): Promise<ITask[]>;
+  getAllIntervalTasks(): Promise<IIntervalTask[]>;
+
+  getCronTaskById(id: string): Promise<ICronTask | undefined>;
+
+  getOnceTimeTaskById(id: string): Promise<IOnceTimeTask | undefined>;
+
+  getIntervalTaskById(id: string): Promise<IIntervalTask | undefined>;
+
+  updateCronTask(id: string, updates: Partial<ICronTask>): Promise<void>;
+
+  updateOnceTimeTask(id: string, updates: Partial<IOnceTimeTask>): Promise<void>;
+
+  updateIntervalTask(id: string, updates: Partial<IIntervalTask>): Promise<void>;
+
+  deleteCronTask(id: string): Promise<void>;
+
+  deleteOnceTimeTask(id: string): Promise<void>;
+
+  deleteIntervalTask(id: string): Promise<void>;
 }
 
-export type { ITask, ITaskDatabase, ICronTask };
+export type { ITask, ITaskDatabase, ICronTask, IIntervalTask, IOnceTimeTask };
 
 export { TaskType };
