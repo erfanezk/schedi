85d49fc3560083ddc95f4f5350f7437dad129395
erfan <erfanrunc@gmail.com>
2025-02-16 19:18:41 +0330

    test: add new function for mocking task

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,7 +1,23 @@
+import { IIntervalTask } from '@/interfaces';
+
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 function secondsToMilliseconds(seconds: number) {
   return seconds * 1000;
 }
 
-export { wait, secondsToMilliseconds };
+const generateMockTask = (overrides: Partial<IIntervalTask> = {}): IIntervalTask => ({
+  id: `task-${Math.random()}`,
+  interval: 1000,
+  startAt: Date.now(),
+  expireAt: Date.now() + secondsToMilliseconds(100),
+  callback: jest.fn(),
+  name: 'task',
+  createdAt: Date.now(),
+  totalRunCount: 0,
+  lastRunAt: undefined,
+  enabled: true,
+  ...overrides,
+});
+
+export { wait, secondsToMilliseconds, generateMockTask };
