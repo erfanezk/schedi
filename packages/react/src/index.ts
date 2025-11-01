89ef266954a036cf37345eb357d5872c009f9131
erfanezk <erfan.ezkat@gmail.com>
2025-11-01 03:51:21 -0700

    chore(react): remove placeholder code
    Remove temporary placeholder code from react package entry point

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,13 +3,4 @@
  * React bindings for schedi
  */
 
-import { greeting } from '@schedi/core';
-
-export const useSchedi = () => {
-  return {
-    message: `${greeting()} - React integration ready!`,
-  };
-};
-
-export { greeting };
-
+export {}
